9286dedac736ca8c977c25d5c637749b237866d1
riteshp224 <109511520+riteshp224@users.noreply.github.com>
2023-12-06 15:15:03 +0530

    Update Program.cs

--- QuoteManagement.WebApi/Program.cs
+++ QuoteManagement.WebApi/Program.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 namespace QuoteManagement.WebApi
 {
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
             var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
